fd5527d4541ded0355bc7a005fda518c424286de
Xact <70323633+Reshwin30@users.noreply.github.com>
2023-12-31 15:19:47 +0530

    .

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LazyDeveloperr/LazyPrincess.git /LazyPrincess
+  git clone https://github.com/Reshwin30/autofilter_filestore.git /autofilter_filestore
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincess
+  git clone $UPSTREAM_REPO /autofilter_filestore
 fi
 cd /LazyPrincess
 pip3 install -U -r requirements.txt
